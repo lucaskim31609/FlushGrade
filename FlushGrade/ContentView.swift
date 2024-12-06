59568b44652b7f9d9e25e68d93e6b896a3fe773a
Lucas Kim <89923655+lucaskim31609@users.noreply.github.com>
2024-12-06 01:37:08 -0500

    Initial Commit

---
+++ FlushGrade/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlushGrade
+//
+//  Created by Lucas Kim on 12/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlushGrade/FlushGradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlushGradeApp.swift
+//  FlushGrade
+//
+//  Created by Lucas Kim on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FlushGradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
